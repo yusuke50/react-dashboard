fd4fd8fcd8483afdeb280a3789242da0f2e624a1
yusuke050 <yusuke050@gmail.com>
2025-03-24 00:10:24 +0800

    "daisyUI"

--- src/components/CheckboxItem.jsx
+++ src/components/CheckboxItem.jsx
@@ -8,12 +8,9 @@ const CheckboxItem = ({ value, id, name, checked, onChange, label }) => {
         name={name}
         checked={checked}
         onChange={onChange}
-        className='w-4 h-4 text-theme-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-theme-500 dark:focus:ring-theme-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
+        className='checkbox checkbox-primary'
       />
-      <label
-        htmlFor={id}
-        className='ms-2 text-sm font-medium text-gray-900 dark:text-gray-300'
-      >
+      <label htmlFor={id} className='ms-2 text-sm font-medium'>
         {label}
       </label>
     </div>
--- src/components/InputItem.jsx
+++ src/components/InputItem.jsx
@@ -10,8 +10,8 @@ const InputItem = ({
 }) => {
   return (
     <input
+      className='input input-primary mb-2'
       type={type}
-      className='mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-theme-500 focus:border-theme-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-theme-500 dark:focus:border-theme-500'
       placeholder={placeholder}
       value={value}
       onChange={onChange}
